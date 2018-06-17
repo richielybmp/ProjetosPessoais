5f6048c826c124e273e8f768fb423a0b1f98827f
richielybmp <richielybmp@hotmail.com>
2018-06-17 11:50:22 -0300

    Aprendendo Interpolation e Property Binding

---
+++ Curso-Angular/data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url = "http://github.com/richielybmp";
+
+  cursoAngular = true;
+
+  urlImagem = "https://picsum.photos/200/300/?random";
+
+  constructor() { }
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  ngOnInit
+
+
+}
